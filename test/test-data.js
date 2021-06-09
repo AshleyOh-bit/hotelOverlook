202c72acb0dd97863f04b9c39e6bec0baec16417
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-06-09 17:54:15 -0600

    Add rooms test data

---
+++ test/test-data.js
@@ -0,0 +1,50 @@
+//Rooms
+  let rooms = [
+    {
+    "number": 101,
+    "roomType": "suite",
+    "bidet": false,
+    "bedSize": "queen",
+    "numBeds": 1,
+    "costPerNight": 154.77
+  },
+  {
+    "number": 102,
+    "roomType": "single room",
+    "bidet": false,
+    "bedSize": "full",
+    "numBeds": 2,
+    "costPerNight": 325.88
+  },
+  {
+    "number": 103,
+    "roomType": "junior suite",
+    "bidet": true,
+    "bedSize": "king",
+    "numBeds": 1,
+    "costPerNight": 267.05
+  },
+  {
+    "number": 104,
+    "roomType": "residential suite",
+    "bidet": true,
+    "bedSize": "queen",
+    "numBeds": 1,
+    "costPerNight": 406.29
+  },
+  {
+    "number": 105,
+    "roomType": "suite",
+    "bidet": false,
+    "bedSize": "queen",
+    "numBeds": 1,
+    "costPerNight": 344.66
+  }
+]
+
+
+//Customers
+
+
+
+//Bookings
