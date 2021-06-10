2e8038629ae1c753eb0c2f73e2402e2d009f2e5c
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-06-10 15:51:03 -0600

    Add setup to Manager-test

--- test/Manager-test.js
+++ test/Manager-test.js
@@ -1,1 +1,21 @@
+// import { expect } from 'chai';
+import chai from 'chai';
+const expect = chai.expect;
+import User from '../src/User';
+import Customer from '../src/classes/Customer';
+import Manager from '../src/classes/Manager';
+import { rooms, customers, bookings } from './test-data';
 
+describe('Manager', function() {
+  let manager;
+  beforeEach(() => {
+    //Instantiate all data here?
+    manager = new Manager(000, "Hambun Grettibean", "IamTheManager", "00000", "2020/04/01")
+  });
+  it.skip('should be a function', function() {
+    expect(Manager).to.be.a.function();
+  });
+  it.skip('should be an instance of User', function() {
+    expect(manager).to.be.an.instanceof(Manager);
+  });
+});
