1188b0b634d29b0b905740f3dc4f32486823f641
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-06-11 10:55:57 -0600

    Add setCredentials method

--- src/classes/User.js
+++ src/classes/User.js
@@ -3,6 +3,20 @@ class User {
     this.id = num;
     this.name = name;
     this.isLoggedIn = false;
+    this.username = null;
+    this.password = null;
+  }
+  setCredentials(username, password) {
+    if ((!username && !password) || (username.includes(" ") && password.includes(" "))) {
+      return `Please enter a username and password with letters, numbers, and symbols`
+    } else if (!username || username.includes(" ")) {
+      return `Please enter a username with letters, numbers, and symbols`
+    } else if (!password || password.includes(" ")) {
+      return `Please enter a password with letters, numbers, and symbols`
+    } else {
+      this.username = username;
+      this.password = password
+    }
   }
 }
 
--- test/User-test.js
+++ test/User-test.js
@@ -27,32 +27,42 @@ describe('User', function() {
   ///////Methods
   //Setting username and password
   //Happy
-  it.skip('should be able to set username and password', function() {
+  it('should be able to set username and password', function() {
     user.setCredentials("BrandyBoo22", "12345")
 
     expect(user.username).to.equal("BrandyBoo22");
     expect(user.password).to.equal("12345");
   });
   //Sad
-  it.skip('should be not able to set username with an empty string', function() {
+  it('should be not able to set username with an empty string', function() {
     user.setCredentials("", "12345")
 
     expect(user.setCredentials("", "12345")).to.equal(`Please enter a username with letters, numbers, and symbols`);
   });
-  it.skip('should be not able to set password with an empty string', function() {
+  it('should be not able to set password with an empty string', function() {
     user.setCredentials("BrandyBoo22", "")
 
     expect(user.setCredentials("BrandyBoo22", "")).to.equal(`Please enter a password with letters, numbers, and symbols`);
   });
-  it.skip('should not be able to set username with a space in it', function() {
+  it('should be not able to set username and password with empty strings', function() {
+    user.setCredentials("", "")
+
+    expect(user.setCredentials("", "")).to.equal(`Please enter a username and password with letters, numbers, and symbols`);
+  });
+  it('should not be able to set username with a space in it', function() {
     user.setCredentials("Brandy Boo22", "12345")
 
     expect(user.setCredentials("Brandy Boo22", "12345")).to.equal(`Please enter a username with letters, numbers, and symbols`);
   });
-  it.skip('should not be able to set password with a space in it', function() {
+  it('should not be able to set password with a space in it', function() {
     user.setCredentials("BrandyBoo22", "123 45")
 
-    expect(user.setCredentials("Brandy Boo22", "123 45")).to.equal(`Please enter a password with letters, numbers, and symbols`);
+    expect(user.setCredentials("BrandyBoo22", "123 45")).to.equal(`Please enter a password with letters, numbers, and symbols`);
+  });
+  it('should not be able to set a username or password with a space in it', function() {
+    user.setCredentials("Brandy Boo22", "123 45")
+
+    expect(user.setCredentials("Brandy Boo22", "123 45")).to.equal(`Please enter a username and password with letters, numbers, and symbols`);
   });
 
   //Logging in
