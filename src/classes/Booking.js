4fc28879346718ea755ae41789df15fac9800c91
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-06-11 10:31:27 -0600

    Add generateRandomId method

--- src/classes/Booking.js
+++ src/classes/Booking.js
@@ -1,7 +1,46 @@
 class Booking {
-  constructor() {
-    
+  constructor(idNum, dateBooked, roomNum) {
+    this.id = null;
+    this.userID = idNum;
+    this.date = dateBooked;
+    this.roomNumber = roomNum;
+    this.roomServiceCharges = [];
+
   }
+  generateRandomId(bookings) {
+    let newId;
+    let template = "8kjhdia80caplask5";
+    template = template.split("")
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+      // for ( var i = 0; i < 17; i++ ) {
+      //   newId += characters.charAt(Math.floor(Math.random() *
+      //  characters.length));
+      //   }
+    const createRandomString = () => {
+      newId = "";
+      const fresh = template.map(letter => {
+        //console.log("old", letter)
+        return letter = characters.charAt(Math.floor(Math.random() *
+       characters.length))
+       //console.log("new", letter)
+      })
+      newId = fresh.join("")
+      //console.log(newId)
+    }
+    //newId.join()
+    createRandomString()
+
+    if (!bookings.some(booking => {
+      booking.id === newId
+    })) {
+      this.id = newId;
+    } else {
+      createRandomString()
+    }
+  }
+
+
 }
 
 
--- test/Booking-test.js
+++ test/Booking-test.js
@@ -14,38 +14,25 @@ describe('Booking', function() {
   it('should be an instance of Booking', function() {
     expect(booking).to.be.an.instanceof(Booking);
   });
-  it.skip('should store the user\'s id number', function() {
+  it('should store the user\'s id number', function() {
     expect(booking.userID).to.equal(227);
   });
-  it.skip('should store the date to be booked', function() {
+  it('should store the date to be booked', function() {
     expect(booking.date).to.equal("2020/03/28");
   });
-  it.skip('should store the date to be booked', function() {
+  it('should store the date to be booked', function() {
     expect(booking.roomNumber).to.equal(105);
   });
-  it.skip('should be prepared to store room service charges', function() {
+  it('should be prepared to store room service charges', function() {
     expect(booking.roomServiceCharges).to.deep.equal([]);
   });
-  it.skip('should have an id', function() {
+  it('should have an id', function() {
     expect(booking.id).to.equal(null);
   });
   //Methods
-  it.skip('should generate a random id', function() {
-    //Make sure to check all bookings to check if available?
+  it('should generate a random id', function() {
+    //Make sure to check all bookings to check if id available?
     booking.generateRandomId(bookings);
     expect(booking.id.length).to.equal(17);
   });
-
-//   function makeid(length) {
-//     var result           = '';
-//     var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-//     var charactersLength = characters.length;
-//     for ( var i = 0; i < length; i++ ) {
-//       result += characters.charAt(Math.floor(Math.random() *
-//  charactersLength));
-//    }
-//    return result;
-// }
-//
-// console.log(makeid(5));
 });
