dec6d82bf4577ceb989fb699201a208395025a2e
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-06-12 18:43:45 -0600

    Add tab index to cards

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -10,7 +10,7 @@ const domUpdates = {
   populateRoomArray(array, location) {
       array.forEach(element => {
         location.innerHTML +=
-        `<article class="booking-card">
+        `<article class="booking-card" tabindex=0>
           <article class="room-details">
             <h3>RoomType: ${element.roomType}</h3>
             <p>Room number: ${element.number}</p>
@@ -26,7 +26,7 @@ const domUpdates = {
     populateBookingArray(array, location) {
         array.forEach(element => {
           location.innerHTML +=
-          `<article class="booking-card">
+          `<article class="booking-card" tabindex=0>
             <article class="room-details">
               <h3>Your ID: ${element.userID}</h3>
               <p>Date Booked: ${element.date}</p>
